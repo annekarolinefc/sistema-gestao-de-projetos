07b27fa22e2eccf0f79f8b97a57055681effcdda
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-09 16:49:12 -0300

    Componentes

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+//criando o componente
+import Frase from "./Frase"
+//criando o componente
+function HelloWorld(){
+    return (
+        <div>
+            <h1> Meu primeiro Componente </h1>
+            <Frase />
+        </div>
+    )
+}
+
+
+//exportando o componente
+export default HelloWorld
